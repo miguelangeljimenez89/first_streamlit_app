41fd44b997b4e8646538ad5d978fff5246ac4415
Miguel Angel Jimenez R <78062633+miguelangeljimenez89@users.noreply.github.com>
2022-08-12 16:15:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ streamlit.title('Ejemplo Stremalit')
 
 streamlit.header('Menu')
 streamlit.text('🥗 Hamburguesa')
-stremalit.text('🥗 Chicken grill 🐔')
+#stremalit.text('🥗 Chicken grill 🐔')
 streamlit.text('🥗 Hot Dog 🍞')
 streamlit.text('🥗 Corn & bacon & 🥑')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
